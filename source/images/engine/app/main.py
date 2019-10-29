8fbea28f78736df4bba1691b9f6acfca3f9049c5
Dmitry Kulikov <jubbon@yandex.ru>
2019-10-29 15:12:25 +0300

    Добавлены Docker-образы сервера и клиента Kafka

---
+++ source/images/engine/app/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bus.ping import app as app_ping
+
+
+def main():
+    '''
+    '''
+    app_ping.main()
---
+++ source/images/fetcher.ping/app/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import asyncio
+from bus.ping import ping
+
+
+async def send_value(interval: int) -> None:
+    '''
+    '''
+    i = 0
+    while True:
+        message = f"[{i}] Hello, I am alive"
+        print(f"Send message: '{message}'", flush=True)
+        ping.cast(value=message)
+        i += 1
+        await asyncio.sleep(interval)
+
+
+def main():
+    '''
+    '''
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_value(15))
