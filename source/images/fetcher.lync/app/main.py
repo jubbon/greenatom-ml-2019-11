f56251feec971300009c30b3b65ed40414f285db
Dmitry Kulikov <jubbon@yandex.ru>
2019-11-05 19:03:20 +0300

    Добавлен Docker-образ fetcher.lync для сбора данных из Skype for business

---
+++ source/images/fetcher.lync/app/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import asyncio
+from datetime import datetime
+from uuid import uuid4
+
+from bus.models.lync import handler, Message
+
+
+async def fetch_messages(every: int) -> None:
+    '''
+    '''
+    while True:
+        message = Message(
+            uid=str(uuid4()),
+            sender="Dmitry",
+            recipient="Aidar",
+            sended_at=datetime(2019, 10, 13, 12, 34, 56),
+            received_at=datetime(2019, 10, 13, 14, 24, 18),
+            text="Привет! Есть вопрос по проекту Омега")
+        print(f"Send message '{message.uid}'", flush=True)
+        handler.cast(value=message)
+        await asyncio.sleep(every)
+
+
+def main():
+    '''
+    '''
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fetch_messages(every=5))
