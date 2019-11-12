3a484e8386d5d784813a3dfb86aafd43406d4bad
Dmitry Kulikov <jubbon@yandex.ru>
2019-11-12 09:58:22 +0300

    Добавлена заготовка Docker-контейнера для получения данных из Exchange

---
+++ source/images/fetchers/fetcher.exchange/app/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    '''
+    '''
+    import time
+    while True:
+        print("Fetching data from Exchange", flush=True)
+        time.sleep(60)
