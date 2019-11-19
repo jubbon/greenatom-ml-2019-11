1b42ef85da7bd1d29e4c7ba6f807c252b00a079d
Aidar Sayfullin <aidar.saifoulline@gmail.com>
2019-11-19 09:03:31 +0300

    реализован код компонента рассылки e-mail сообщений

--- source/images/mailer/app/main.py
+++ source/images/mailer/app/main.py
@@ -3,6 +3,8 @@
 
 import os
 
+from mailer import Mailer, Message
+
 from smart_hr.bus.app import app
 from smart_hr.bus.models.email import topic
 
@@ -13,7 +15,14 @@ async def handler(messages):
         to = os.getenv("SMTP_REDIRECT_TO", message.to)
         print(f"Send email '{message.text}' to '{to}'", flush=True)
         try:
-            pass
+            msg = Message()
+            msg.From = "SmartHR Notify Service <SmartHR@inbox.ru>"
+            msg.To = to
+            msg.Subject = "Список сотрудников-экспертов"
+            msg.Html = message.text
+
+            sender = Mailer('smtp.mail.ru', port=465, use_ssl=True, usr='SmartHR@inbox.ru', pwd='greenatomhackathon')
+            sender.send(msg)
         except Exception as err:
             print(f"[ERROR]: {err}", flush=True)
         yield "OK"
